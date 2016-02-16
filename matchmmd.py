34247e20ee7ed75b21a90a3c88d6285dd836fa9a
Paul Upchurch <paulup@gmail.com>
2016-02-16 14:24:56 -0500

    experimental nopca code

--- matchmmd.py
+++ matchmmd.py
@@ -12,7 +12,7 @@ import threading
 import minimize
 import threadparallel
 
-def witness_fn(r,x,P,Q,rbf_var,weight,kernel):
+def witness_fn(r,x,P,Q,rbf_var,weight):
   # r is D dim
   # x is D dim
   # P is N x D, source distribution
@@ -28,34 +28,26 @@ def witness_fn(r,x,P,Q,rbf_var,weight,kernel):
   N=P.shape[0]
   M=Q.shape[0]
   x=x+r
-  if kernel=='l2':
+  if N>0:
     xmP=x-P
     xmQ=x-Q
-    loss=0.5*((xmQ**2).sum()/M-(xmP**2).sum()/N)+(r**2).sum()*weight
-    grad=xmQ.sum(axis=0)/M-xmP.sum(axis=0)/N+2*r*weight
-  elif kernel=='rbf':
-    if N>0:
-      xmP=x-P
-      xmQ=x-Q
-      #print('xmP',xmP.shape,xmP.dtype,xmP.min(),xmP.max())
-      #print('xmQ',xmQ.shape,xmQ.dtype,xmQ.min(),xmQ.max())
-      kxP=np.exp(-(xmP**2).sum(axis=1)/(2*rbf_var))
-      kxQ=np.exp(-(xmQ**2).sum(axis=1)/(2*rbf_var))
-      #print('kxP',kxP.shape,kxP.dtype,kxP.min(),kxP.max())
-      #print('kxQ',kxQ.shape,kxQ.dtype,kxQ.min(),kxQ.max())
-      assert kxP.shape==(N,)
-      assert kxQ.shape==(M,)
-      loss=kxP.sum()/N-kxQ.sum()/M+(r**2).sum()*weight
-      grad=(-kxP.reshape(N,1)*xmP/N/rbf_var).sum(axis=0)+(kxQ.reshape(M,1)*xmQ/M/rbf_var).sum(axis=0)+2*r*weight
-    else:
-      # source set is empty
-      xmQ=x-Q
-      kxQ=np.exp(-(xmQ**2).sum(axis=1)/(2*rbf_var))
-      assert kxQ.shape==(M,)
-      loss=kxQ.sum()/M+(r**2).sum()*weight
-      grad=(kxQ.reshape(M,1)*xmQ/M/rbf_var).sum(axis=0)+2*r*weight
+    #print('xmP',xmP.shape,xmP.dtype,xmP.min(),xmP.max())
+    #print('xmQ',xmQ.shape,xmQ.dtype,xmQ.min(),xmQ.max())
+    kxP=np.exp(-(xmP**2).sum(axis=1)/(2*rbf_var))
+    kxQ=np.exp(-(xmQ**2).sum(axis=1)/(2*rbf_var))
+    #print('kxP',kxP.shape,kxP.dtype,kxP.min(),kxP.max())
+    #print('kxQ',kxQ.shape,kxQ.dtype,kxQ.min(),kxQ.max())
+    assert kxP.shape==(N,)
+    assert kxQ.shape==(M,)
+    loss=kxP.sum()/N-kxQ.sum()/M+(r**2).sum()*weight
+    grad=(-kxP.reshape(N,1)*xmP/N/rbf_var).sum(axis=0)+(kxQ.reshape(M,1)*xmQ/M/rbf_var).sum(axis=0)+2*r*weight
   else:
-    raise ValueError('Unsupported kernel: {}'.format(kernel))
+    # source set is empty
+    xmQ=x-Q
+    kxQ=np.exp(-(xmQ**2).sum(axis=1)/(2*rbf_var))
+    assert kxQ.shape==(M,)
+    loss=kxQ.sum()/M+(r**2).sum()*weight
+    grad=(kxQ.reshape(M,1)*xmQ/M/rbf_var).sum(axis=0)+2*r*weight
   #print('loss',loss)
   #print('grad',grad.shape,grad.dtype,grad.min(),grad.max())
   #print('r',r.shape,r.dtype,r.min(),r.max(),np.linalg.norm(r))
@@ -108,21 +100,100 @@ def match_distribution(x,P,Q,weights,max_iter=5,rbf_var=1e4):
       def g(*args):
         return witness_fn(*args)[1]
       print('Checking gradient ...')
-      print(scipy.optimize.check_grad(f,g,r[:10],*(x[:10],P[:10,:10],Q[:10,:10],rbf_var,weight,'rbf')))
+      print(scipy.optimize.check_grad(f,g,r[:10],*(x[:10],P[:10,:10],Q[:10,:10],rbf_var,weight)))
     if parallel>1:
       assert (len(P) % parallel)==0
       assert (len(Q) % parallel)==0
-      def witness_fn_parallel(r,x,P,Q,rbf_var,weight,kernel):
-        result=threadparallel.unordered_parallel_call([witness_fn]*parallel,[(r,x,P[i*len(P)//parallel:(i+1)*len(P)//parallel],Q[i*len(Q)//parallel:(i+1)*len(Q)//parallel],rbf_var,weight,kernel) for i in range(parallel)],None)
+      def witness_fn_parallel(r,x,P,Q,rbf_var,weight):
+        result=threadparallel.unordered_parallel_call([witness_fn]*parallel,[(r,x,P[i*len(P)//parallel:(i+1)*len(P)//parallel],Q[i*len(Q)//parallel:(i+1)*len(Q)//parallel],rbf_var,weight) for i in range(parallel)],None)
         loss=sum(x[0] for x in result)
         grad=sum(x[1] for x in result)
         return loss,grad
       r_opt,loss_opt,iter_opt=minimize.minimize(r,witness_fn_parallel,(x,P,Q,rbf_var,weight,'rbf'),maxnumlinesearch=50,maxnumfuneval=None,red=1.0,verbose=True)
     else:
-      r_opt,loss_opt,iter_opt=minimize.minimize(r,witness_fn,(x,P,Q,rbf_var,weight,'rbf'),maxnumlinesearch=50,maxnumfuneval=None,red=1.0,verbose=True)
+      r_opt,loss_opt,iter_opt=minimize.minimize(r,witness_fn,(x,P,Q,rbf_var,weight),maxnumlinesearch=50,maxnumfuneval=None,red=1.0,verbose=True)
     print('r_opt',r_opt.shape,r_opt.dtype,r_opt.min(),r_opt.max(),np.linalg.norm(r_opt))
     print(r_opt[:10])
     x_result.append((x+r_opt)*sigma+loc)
     r_result.append(r_opt*sigma)
   return x_0,np.asarray(x_result),np.asarray(r_result)
+
+def witness_fn2(r,x,FFT,N,M,rbf_var,weight,verbose,checkrbf):
+  # K is N+M+1
+  # r is K dim, indicator vector
+  # x is K dim, indicator vector
+  # F is K x D, latent space vectors
+  # FFT is F F^T
+  K=N+M+1
+  assert r.shape==(K,)
+  assert x.shape==(K,)
+  assert FFT.shape==(K,K)
+  if verbose:
+    print('r',r.shape,r.dtype,r.min(),r.max())
+    print('x',x.shape,x.dtype,x.min(),x.max())
+
+  P=np.eye(N,K)
+  Q=np.concatenate([np.zeros((M,N)),np.eye(M,M+1)],axis=1)
+  xpr=x+r
+  xmP=xpr.reshape(1,K)-P # N x K
+  xmQ=xpr.reshape(1,K)-Q # M x K
+  AP=xmP.dot(FFT)
+  AQ=xmQ.dot(FFT)
+  Z=(-1.0/(2*rbf_var))
+  eP=Z*(AP.dot(xmP.T)).sum(axis=1)
+  eQ=Z*(AQ.dot(xmQ.T)).sum(axis=1)
+  if checkrbf:
+    if eP.mean()<-10 or eQ.mean()<-10:
+      print('WARNING: rbf_var is too small (eP.mean()={}, eQ.mean={})'.format(eP.mean(),eQ.mean()))
+  KP=np.exp(eP)
+  KQ=np.exp(eQ)
+  B=FFT.dot(r)
+
+  loss=(1.0/N)*KP.sum()-(1.0/M)*KQ.sum()+weight*(r.dot(B))
+  grad=(1.0/N)*Z*(KP.reshape(N,1)*(AP.T.sum(axis=1).reshape(1,K)+N*AP)).sum(axis=0)-(1.0/M)*Z*(KQ.reshape(M,1)*(AQ.T.sum(axis=1).reshape(1,K)+M*AQ)).sum(axis=0)+2*weight*B
+
+  if verbose:
+    print('loss',loss)
+    print('grad',grad.shape,grad.dtype,grad.min(),grad.max())
+  assert grad.shape==r.shape
+  return loss,grad
+
+def manifold_traversal(F,N,M,weights,max_iter=5,rbf_var=1e4,verbose=True,checkgrad=True,checkrbf=True):
+  # returns two arrays, xpr and r
+  #   xpr is optimized x+r
+  #   r is optimized r
+  # multiply by F to get latent space vector
+  if verbose:
+    print('manifold_traversal()')
+    print('F',F.shape,F.dtype,F.min(),F.max())
+    print('N',N)
+    print('M',M)
+    print('weights',weights)
+
+  xpr_result=[]
+  r_result=[]
+  r=np.zeros(len(F))
+  x=np.zeros(len(F))
+  FFT=F.dot(F.T) # K x K
+  x[-1]=1
+  for weight in weights:
+
+    if checkgrad:
+      def f(*args):
+        return witness_fn2(*args)[0]
+      def g(*args):
+        return witness_fn2(*args)[1]
+      print('Checking gradient ...')
+      err=scipy.optimize.check_grad(f,g,r,*(x,FFT,N,M,rbf_var,weight,False,True))
+      print('gradient error',err)
+      assert err<1e-5
+
+    r_opt,loss_opt,iter_opt=minimize.minimize(r,witness_fn2,(x,FFT,N,M,rbf_var,weight,verbose,checkrbf),maxnumlinesearch=50,maxnumfuneval=None,red=1.0,verbose=True)
+    if verbose:
+      print('r_opt',r_opt.shape,r_opt.dtype,r_opt.min(),r_opt.max(),np.linalg.norm(r_opt))
+      print('r_opt values',r_opt[:5],'...',r_opt[N:N+5],'...',r_opt[-1])
+    xpr_result.append(x+r_opt)
+    r_result.append(r_opt)
+    r=r_opt
+  return np.asarray(xpr_result),np.asarray(r_result)
 
---
+++ script_python2.py
@@ -0,0 +1,91 @@
+
+## Imports
+
+import numpy
+np=numpy
+import sys
+import pickle
+import time
+import os
+import os.path
+import subprocess
+import pipes
+import h5py
+import itertools
+
+import matchmmd
+from gen_deepart import read_lfw_attributes,attr_pairs
+
+## Read P, Q, X, weights and layers
+
+attr=10
+source_k=2000
+target_k=2000
+test_indices=sorted([6005, 3659, 8499, 12217, 9982, 4322, 10449, 10969, 4245, 7028])
+test_indices=[0]
+_,_,lfwattr=read_lfw_attributes()
+if attr>=0:
+  target_indices,source_indices=attr_pairs(lfwattr,attr,target_k,source_k)
+else:
+  source_indices,target_indices=attr_pairs(lfwattr,-attr,source_k,target_k)
+
+P=sorted(source_indices) # list of indices (source distribution)
+Q=sorted(target_indices) # list of indices (target distribution)
+X=test_indices   # list of indices (test images)
+weights=[8e-8,4e-8,2e-8,1e-8]
+
+## Form F (first N rows are P, next M rows are Q, last row is x)
+
+prefix='data'
+blob_names=['conv3_1','conv4_1','conv5_1']
+h5f={}
+shape={}
+for k in blob_names:
+  assert os.path.exists('{}_{}.h5'.format(prefix,k))
+  h5f[k]=h5py.File('{}_{}.h5'.format(prefix,k),'r')
+  print('h5f',k,h5f[k]['DS'].shape,h5f[k]['DS'].dtype)
+  N=h5f[k]['DS'].shape[0]
+  shape[k]=h5f[k]['DS'].shape[1:]
+
+PF=numpy.concatenate([h5f[k]['DS'][P].reshape(len(P),-1) for k in blob_names],axis=1)
+QF=numpy.concatenate([h5f[k]['DS'][Q].reshape(len(Q),-1) for k in blob_names],axis=1)
+XF=numpy.concatenate([h5f[k]['DS'][X].reshape(len(X),-1) for k in blob_names],axis=1)
+for k in blob_names:
+  h5f[k].close()
+
+F=numpy.concatenate([PF,QF,XF[:1]],axis=0)
+print 'F',F.shape,F.dtype
+del h5f,PF,QF
+
+## Solve for multiple points on the manifold (move away from P toward Q)
+
+reload(matchmmd)
+allF2=[]
+for x in XF:
+  F[-1]=x
+  XPR,R=matchmmd.manifold_traversal(F,len(P),len(Q),weights,rbf_var=1e10,checkgrad=False,checkrbf=True)
+  print('R',R.shape,R.dtype)
+  allF2.append(XPR.dot(F))
+  XPR2=numpy.copy(XPR)
+  XPR2[:,:len(P)]=XPR[:,:len(P)].mean(axis=1,keepdims=True)
+  XPR2[:,len(P):len(P)+len(Q)]=XPR[:,len(P):len(P)+len(Q)].mean(axis=1,keepdims=True)
+
+prefix='out2'
+F2=np.asarray(allF2,dtype=np.float32)
+F2=F2.reshape(F2.shape[0]*F2.shape[1],-1)
+print('F2',F2.shape,F2.dtype,F2.min(),F2.max())
+index=0
+for k in blob_names:
+  size=numpy.prod(shape[k])
+  h5f=h5py.File('{}_{}.h5'.format(prefix,k),'w')
+  h5f.create_dataset('DS',data=F2[:,index:index+size].reshape(-1,*shape[k]))
+  h5f.close()
+  index=index+size
+
+## Reconstruct input from points in latent space
+
+from gen_deepart import deepart_reconstruct
+
+test_indices2=test_indices*len(weights)
+deepart_reconstruct(blob_names=blob_names,blob_weights=[1]*len(blob_names),prefix=prefix,max_iter=3000,test_indices=test_indices2,data_indices=None,image_dims=(125,125),hybrid_names=['conv1_1','conv2_1'],hybrid_weights=[0.02,0.02],dataset='lfw',desc='testpy')
+
